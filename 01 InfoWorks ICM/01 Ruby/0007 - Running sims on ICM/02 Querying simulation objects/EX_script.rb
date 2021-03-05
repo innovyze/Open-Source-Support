587077667cbe62a123af1ef6bb5f4e986303cab2
Daniel Moreira <de8a88@gmail.com>
2021-03-05 13:47:15 +0000

    refactored and added rainfall

--- 01 InfoWorks ICM/01 Ruby/0007 - Running sims on ICM/02 Querying simulation objects/EX_script.rb
+++ 01 InfoWorks ICM/01 Ruby/0007 - Running sims on ICM/02 Querying simulation objects/EX_script.rb
@@ -4,17 +4,26 @@ $db_name="2021.1.1_Standalone.icmm"
 $db_file="#{$project_path}\\#{$db_name}"
 $db=WSApplication.open $db_file,false
 
+def find_name(type,id)
+	$db.model_object_from_type_and_id(type,id).name
+end
+
 # Start script
 group=$db.model_object '>MODG~Model group'
 group.children.each do |run|
 	if run.type=='Run'
-		model_id = run['Model Network']
-		level_id = run['Level']
+		net_id = run['Model Network']
+		lev_id = run['Level']
 		commit = run['Model Network Commit ID']
+		rainfalls=Array.new
+		run.children.each { |sim| rainfalls << sim['Rainfall event'] }
 		puts "Run parameters:"
-		puts "=> Model network name:        #{$db.model_object_from_type_and_id('Model Network',model_id).name}"
-		puts "=> Model commit version:      #{commit}"
-		puts "=> Level file used:           #{$db.model_object_from_type_and_id('Level',level_id).name}"
-		puts "=> Simulation duration:       #{run['Duration']}"
+		puts "=> Model network name: #{find_name('Model network',net_id)}"
+		puts "=> Model commit version: #{commit}"
+		puts "=> Level file used: #{find_name('Level',lev_id)}"
+		rainfalls.each do |rain_id| 
+			puts "=> Rainfall file: #{find_name('Rainfall Event',rain_id)}"
+		end
+		puts "=> Simulation duration: #{run['Duration']}"
 	end
 end
