b4975b60b53cd4f1f76b7758974e8de1232cab99
Daniel Moreira <de8a88@gmail.com>
2021-03-05 12:38:36 +0000

    refactored and added gif

--- 01 InfoWorks ICM/01 Ruby/0007 - Running sims on ICM/03 Rerunning existing simulation/EX_script.rb
+++ 01 InfoWorks ICM/01 Ruby/0007 - Running sims on ICM/03 Rerunning existing simulation/EX_script.rb
@@ -7,16 +7,14 @@ $db=WSApplication.open $db_file,false
 # Rerun all sims in the Model Group
 $group=$db.model_object '>MODG~Model group'
 $group.children.each do |run|
+	$sims_array = Array.new
 	if run.type=='Run'
-		sims_array = Array.new
-		run.children.each do |sim|
-			sims_array << sim 
-			WSApplication.connect_local_agent(1)
-			WSApplication.launch_sims sims_array,'.',false,0,0
-			while sim.status=='sNone'
-				puts  'running'
-				sleep 1
-			end
-		end
+		run.children.each { |sim| $sims_array << sim }
+		WSApplication.connect_local_agent(1)
+		WSApplication.launch_sims $sims_array,'.',false,0,0
 	end
+end
+while $sims_array.any? { |sim| sim.status=='None' } 
+	puts  'running'
+	sleep 1
 end
