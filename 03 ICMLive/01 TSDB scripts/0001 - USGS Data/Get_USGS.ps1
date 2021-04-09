58a7db0c03e9b3dac73cd0f9534170835f555d63
Daniel Moreira <de8a88@gmail.com>
2021-04-09 11:42:38 +0100

    minor updates to code

--- 03 ICMLive/01 TSDB scripts/0001 - USGS Data/Get_USGS.ps1
+++ 03 ICMLive/01 TSDB scripts/0001 - USGS Data/Get_USGS.ps1
@@ -1,11 +1,12 @@
-# Global Variables
-$DataPath = "C:\TEMP\"
+# USGS Instantaneous Values Web Service
+# https://waterservices.usgs.gov/rest/IV-Service.html
 $UsgsUrl = "https://waterservices.usgs.gov"
-
-# Sites (examples)
+# User defined variables
+$DataPath = "C:\TEMP\"
+$LevlObsPeriod = 72                         # Period in h for observed level data to be retrieved
+$RainObsPeriod = 72                         # Period in h for observed rainfall data to be retrieved
+# Sites
 # https://maps.waterdata.usgs.gov/mapper/index.html
-$LevlObsPeriod = 72                         # Period for observed level data to be retrieved
-$RainObsPeriod = 72                         # Period for observed rainfall data to be retrieved
 $USGSTidlSites = @(                         # =< Add/Remove as necessary >=
   "01392650"                                # Newark
   "01376562"                                # Great Kills Harbor
@@ -18,15 +19,14 @@ $USGSRainSites = @(                         # =< Add/Remove as necessary >=
   "405245074022401"                         # Hackensack
   "405934074120201"                         # Ridgewood
 )
-
-# Parameter codes                           # https://help.waterdata.usgs.gov/codes-and-parameters/parameters
-$ParamCodes = @{
+# Parameter codes
+# https://help.waterdata.usgs.gov/codes-and-parameters/parameters
+$ParamCodes = @{                            # =< Add as necessary >=
   TidElevation = '72279'
   PhysGageHeight = '00065'
   PhysPrecip = '00045'
 }
-
-# Function to get data in JSON format, convert date time to UTC and export relevant fields to Simple CSV format
+# Function to get data in JSON format, convert date time to UTC and export relevant fields to TSDB "Simple CSV" format
 function Get-Data {
   Param($SiteArray, $ParameterCd, $Period)
   ForEach ($SiteId in $SiteArray) {
@@ -38,20 +38,15 @@ function Get-Data {
       $_.datetime = [datetime]::Parse($_.datetime).ToUniversalTime().ToString("yyyy-MM-dd HH:mm")
       $_
     } |
-    ConvertTo-CSV -NoTypeInformation |
-    ForEach-Object {$_ -replace '"',''} | 
-    Out-File -encoding "ASCII" $OutFile
+    ConvertTo-CSV -NoTypeInformation | ForEach-Object {$_ -replace '"',''} | Out-File -encoding "ASCII" $OutFile
     Write-Output $OutFile
   }
 }
-
 # Create a path for the files if it doesn't exist
 New-Item -ItemType Directory -Force -Path $DataPath | Out-Null
-
 # Call Get-Data function
 Get-Data -SiteArray $USGSTidlSites -ParameterCd $ParamCodes.TidElevation -Period $LevlObsPeriod
 Get-Data -SiteArray $USGSLevlSites -ParameterCd $ParamCodes.PhysGageHeight -Period $LevlObsPeriod
 Get-Data -SiteArray $USGSRainSites -ParameterCd $ParamCodes.PhysPrecip -Period $RainObsPeriod
-
 # Example REST Url
 # https://waterservices.usgs.gov/nwis/iv/?sites=405939074084301&format=rdb&period=PT72H&parameterCd=00045
