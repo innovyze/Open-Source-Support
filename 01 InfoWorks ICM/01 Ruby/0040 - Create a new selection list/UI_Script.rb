d8ed72a4f230883944b64705768f45a1a42e1e41
Daniel Moreira <de8a88@gmail.com>
2021-06-10 13:54:06 +0100

    Update UI_Script.rb

--- 01 InfoWorks ICM/01 Ruby/0040 - Create a new selection list/UI_Script.rb
+++ 01 InfoWorks ICM/01 Ruby/0040 - Create a new selection list/UI_Script.rb
@@ -1,6 +1,6 @@
 db=WSApplication.current_database
 net=WSApplication.current_network
-net.clear_selection
+net.clear_selection 
 group=db.find_root_model_object 'Model Group','New Selection'
 net.run_SQL "_links","link_type = 'cond'"
 sl=group.new_model_object 'Selection List','Conduits'
