85bdd8443953541e12f85f3ffd44b6723cca43f7
Daniel Moreira <de8a88@gmail.com>
2021-06-10 13:50:37 +0100

    updated wrong script

--- 01 InfoWorks ICM/01 Ruby/0040 - Create a new selection list/UI_Script.rb
+++ 01 InfoWorks ICM/01 Ruby/0040 - Create a new selection list/UI_Script.rb
@@ -1,7 +1,7 @@
 db=WSApplication.current_database
 net=WSApplication.current_network
 net.clear_selection
-group=db.model_object_from_type_and_id 'Model Group',5
-net.run_SQL 'hw_node','x>644220'
-sl=group.new_model_object 'Selection List','Andrew'
+group=db.find_root_model_object 'Model Group','New Selection'
+net.run_SQL "_links","link_type = 'cond'"
+sl=group.new_model_object 'Selection List','Conduits'
 net.save_selection sl
