2569fa8c5569350dd444b91ed3a52fbac85eb267
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2021-08-02 15:21:49 +0100

    alignment amendment

--- 02 InfoAsset Manager/01 Ruby/0002A ODIC Callback Examples/ODIC_Importer_PipeTypeChoices.rb
+++ 02 InfoAsset Manager/01 Ruby/0002A ODIC Callback Examples/ODIC_Importer_PipeTypeChoices.rb
@@ -2,18 +2,18 @@
 class Importer
 	def Importer.OnEndRecordPipe(obj)
 
-		if obj['OpsStat'] == '0'												#If source field OpStat=0
-			obj['status'] = 'AB'												#Set IAM Opertaional_status field as choice 'AB'
+		if obj['OpsStat'] == '0'										#If source field OpStat=0
+			obj['status'] = 'AB'										#Set IAM Opertaional_status field as choice 'AB'
 		elsif obj['OpsStat'] == '1'
 			obj['status'] = 'INUSE'
 		elsif obj['OpsStat'] == '2'
 			obj['status'] = 'STANDBY'
 		elsif obj['OpsStat'] == '3'
-			obj['status'] = 'INUSE' and obj['user_text_1'] = 'Temp'				#Set IAM Opertaional_status field as choice 'INUSE' and UT1 as 'TEMP'
+			obj['status'] = 'INUSE' and obj['user_text_1'] = 'Temp'		#Set IAM Opertaional_status field as choice 'INUSE' and UT1 as 'TEMP'
 		end
 
-		if obj['PipeUseCode'] == '0'											#If source field PipeUseCode=0
-			obj['system_type'] = ''												#Set IAM system_type field as ''
+		if obj['PipeUseCode'] == '0'				#If source field PipeUseCode=0
+			obj['system_type'] = ''					#Set IAM system_type field as ''
 		elsif obj['PipeUseCode'] == 'Fo'
 			obj['system_type'] = 'F'
 		elsif obj['PipeUseCode'] == 'Su'
@@ -28,8 +28,8 @@ class Importer
 			obj['system_type'] = 'LD'
 		elsif obj['PipeUseCode'] == 'Other'
 			obj['system_type'] = 'U'
-		else
-			obj['system_type'] = 'U'
+		else										#If none of the above IFs are met
+			obj['system_type'] = 'U'				#Set IAM field as 'U'
 		end
 
 	end
