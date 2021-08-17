8d3728e2cb5a0d9b612ce53438fd426ada0e5bbb
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2021-08-17 12:12:55 +0100

    SharePoint upload

---
+++ 02 InfoAsset Manager/01 Ruby/0019 Distribute attachment details by a shared value/UI-PDFDistribute.rb
@@ -0,0 +1,55 @@
+class Attachments
+	def initialize
+		@net=WSApplication.current_network
+	end
+	def doit
+		map=Hash.new
+		@net.row_objects('cams_cctv_survey').each do |ro|
+			attachments=ro.attachments
+			attachments.each do |a|
+				if a.db_ref.downcase[-4..-1]=='.pdf'
+					if map[ro.contract_no].nil?
+						pdf=Array.new
+						pdf << a.purpose
+						pdf << a.filename
+						pdf << a.description
+						pdf << a.db_ref
+						map[ro.contract_no]=pdf
+					else
+						puts "Duplicate PDF for contract no #{ro.contract_no} in survey #{ro.id}"
+					end
+				end
+			end
+		end
+		@net.transaction_begin
+		@net.row_objects('cams_cctv_survey').each do |ro|
+			if !map.has_key? ro.contract_no
+				puts "Survey #{ro.id} - contract no not matched or can't find PDF"
+			else
+				found=false
+				
+				attachments=ro.attachments				
+				attachments.each do |a|
+					if a.db_ref.downcase[-4..-1]=='.pdf'
+						found=true
+						break
+					end
+				end
+				if !found
+					n=attachments.length
+					attachments.length=n+1
+					attachments[n].purpose=map[ro.contract_no][0]
+					attachments[n].filename=map[ro.contract_no][1]
+					attachments[n].description=map[ro.contract_no][2]
+					attachments[n].db_ref=map[ro.contract_no][3]
+					attachments.write
+					ro.write
+				end
+					
+			end
+		end
+		@net.transaction_commit
+	end
+end
+fred=Attachments.new
+fred.doit
