8ece18e203c166c41db5b392f05ab6917e586e79
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2021-09-24 11:51:11 +0100

    New ODIC/ODEC callback class examples

---
+++ 02 InfoAsset Manager/01 Ruby/0002A ODIC Callback Examples/ODIC_Importer_BooleanConversion.rb
@@ -0,0 +1,12 @@
+## If source fields cover_locable/side_entry have value "Y", set IAM boolean field = true/checked
+class Importer
+        def Importer.onEndRecordNode(obj)
+               if obj['cover_lockable']=='Y'
+                       obj['cover_type_locking']=true
+               end
+
+               if obj['side_entry']=='Y'
+                       obj['side_entry']=true
+               end
+        end
+end
