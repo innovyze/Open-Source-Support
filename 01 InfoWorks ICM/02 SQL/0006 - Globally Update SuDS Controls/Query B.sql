6807cc842ae1daee23f018da6144516e090aa93b
Squark89 <grist.alex@gmail.com>
2021-09-28 16:34:39 +0100

    SQL 006 update

--- 01 InfoWorks ICM/02 SQL/0006 - Globally Update SuDS Controls/Query B.sql
+++ 01 InfoWorks ICM/02 SQL/0006 - Globally Update SuDS Controls/Query B.sql
@@ -1,3 +1,6 @@
+//Object: Subcatchment
+//Spatial Search: blank
+
 LIST $Surface = "All", "Surface 1", "Surface 2", "Surface 3";
 List $Out = "Outlet", "Pervious", "Impervious";
 
@@ -5,6 +8,8 @@ PROMPT LINE  $SS 'suds_controls.suds_structure' STRING LIST $Type;
 PROMPT LINE  $SID 'suds_controls.id' STRING;
 PROMPT LINE  $A 'suds_controls.area %age';
 PROMPT LINE  $NU 'suds_controls.num_units';
+PROMPT LINE  $IAT 'suds_controls.impervious_area_treated_pct';
+PROMPT LINE  $PAT 'suds_controls.pervious_area_treated_pct';
 PROMPT LINE  $O 'suds_controls.outflow_to' LIST $Out;
 PROMPT LINE  $SU 'suds_controls.surface' LIST $Surface;
 
@@ -16,6 +21,8 @@ INSERT INTO subcatchment.suds_controls (
   suds_controls.id,
   suds_controls.area,
   suds_controls.num_units,
+  suds_controls.impervious_area_treated_pct,
+  suds_controls.pervious_area_treated_pct,
   suds_controls.outflow_to,
   suds_controls.surface
 )
@@ -23,8 +30,10 @@ SELECT
   oid,
   $SS,
   $SID,
-  area_absolute_2 * $A * 100,
+  contributing_area * $A * 100,
   $NU,
+  $IAT,
+  $PAT,
   $O,
   $SU
 FROM Subcatchment
