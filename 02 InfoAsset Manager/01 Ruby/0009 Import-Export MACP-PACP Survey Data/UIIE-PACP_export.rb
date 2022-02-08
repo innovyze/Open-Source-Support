e1d3d9150aa11226bb8e8117f7712eaaa2292352
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2022-02-08 12:55:20 +0000

    minor change

--- 02 InfoAsset Manager/01 Ruby/0009 Import-Export MACP-PACP Survey Data/UIIE-PACP_export.rb
+++ 02 InfoAsset Manager/01 Ruby/0009 Import-Export MACP-PACP Survey Data/UIIE-PACP_export.rb
@@ -14,7 +14,7 @@ myHash=Hash.new
 myHash["Selection Only"]=false				## Boolean, true for selection only, all objects otherwise | Default=false
 myHash["Images"]=false						## Boolean, if true the images are exported to same location as .mdb | Default=false
 myHash["Imperial"]=true						## Boolean, true for imperial values (the WSApplication setting for units is ignored) | Default=false
-myHash["InfoAsset"]=4						## nil or an Integer, if an integer must be between 1 and 10 – corresponds to the dialog setting | Default=nil
+myHash["InfoAsset"]=nil						## nil or an Integer, if an integer must be between 1 and 10 – corresponds to the dialog setting | Default=nil
 myHash["Format"]="7"						## String, PACP db version format (must be "6" or "7") | Default=7
 myHash["LogFile"]='C:\\Temp\\PACP-log.txt'	## String, path of a log file, if nil or blank then nothing is logged to the file | Default=nil
 
