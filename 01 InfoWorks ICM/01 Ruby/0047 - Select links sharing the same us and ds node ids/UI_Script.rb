9d54df06e99a3be4436554d9a7117e83daaf6706
Daniel Moreira <de8a88@gmail.com>
2022-02-14 15:52:28 +0000

    Update UI_Script.rb

--- 01 InfoWorks ICM/01 Ruby/0047 - Select links sharing the same us and ds node ids/UI_Script.rb
+++ 01 InfoWorks ICM/01 Ruby/0047 - Select links sharing the same us and ds node ids/UI_Script.rb
@@ -15,7 +15,7 @@ group_by_usds = links_list_all.group_by { |usds| usds.shift }.transform_values {
 # Filters groups with us/ds showing more than once and converts hash to a flattened array of links
 link_list_sel = group_by_usds.select { |key, value| value.length > 1 }.values.flatten
 
-# Selects from the network only links from the list above
+# Selects only links from the list above from the active network
 net.row_objects('_links').each do |link|
   link.selected=true if link_list_sel.include?(link.us_node_id+'.'+link.link_suffix)
 end
