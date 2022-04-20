d11799ee6756125b7b2860f62d8197374b7b3baf
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2022-04-20 16:52:24 +0100

    minor update

--- 02 InfoAsset Manager/02 SQL/0001 Network Tracing/Trace_IntermediateLinks.sql
+++ 02 InfoAsset Manager/02 SQL/0001 Network Tracing/Trace_IntermediateLinks.sql
@@ -8,4 +8,4 @@ UPDATE [All Links] SET $link_selected = 0;
 
 UPDATE SELECTED [All Links] SET $link_selected = 1;
 
-SELECT FROM [All Links] WHERE all_us_links.$link_selected=1 AND all_ds_links.$link_selected=1 
+SELECT FROM [All Links] WHERE all_us_links.$link_selected=1 AND all_ds_links.$link_selected=1;
