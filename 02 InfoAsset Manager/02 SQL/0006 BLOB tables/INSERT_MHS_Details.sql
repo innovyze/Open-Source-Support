fd17b6b3d994aa72a5e3e98f9e7c77dc7a03abfe
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2022-05-10 11:51:56 +0100

    added info

--- 02 InfoAsset Manager/02 SQL/0006 BLOB tables/INSERT_MHS_Details.sql
+++ 02 InfoAsset Manager/02 SQL/0006 BLOB tables/INSERT_MHS_Details.sql
@@ -1,2 +1,6 @@
+/*Create records in the Manhole Survey Details table, Distance=0 and Code='MHS', where the count of details records=0 (no details on the survey currently).
+//Object Type: Manhole Survey
+//Spatial Search: blank
+*/
 INSERT INTO [Manhole Survey].details (id,details.distance,details.code)
 SELECT oid,'0.0','MHS' WHERE count(details.code)=0
