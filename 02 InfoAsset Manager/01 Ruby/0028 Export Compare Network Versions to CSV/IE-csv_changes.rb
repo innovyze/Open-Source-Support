81cdd569217d062180f6f646fbb9b740225862be
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2022-07-08 10:28:18 +0100

    reformat

--- 02 InfoAsset Manager/01 Ruby/0028 Export Compare Network Versions to CSV/IE-csv_changes.rb
+++ 02 InfoAsset Manager/01 Ruby/0028 Export Compare Network Versions to CSV/IE-csv_changes.rb
@@ -1,4 +1,4 @@
 db=WSApplication.open('localhost:40000/database')       ## Open databaswe
-net=db.model_object_from_type_and_id 'Collection Network',20        ## Network to use
+net=db.model_object_from_type_and_id('Collection Network',20)       ## Network to use
 
 net.csv_changes(100,120,'C:\\temp\\changes.csv')        ## nno.csv_changes(commit_id1, commit_id2, filename)
