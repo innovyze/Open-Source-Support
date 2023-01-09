8a5e88ee352c321490aacddf5e262a732dfca3ed
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2023-01-09 18:32:30 +0000

    new scritps added

---
+++ 02 InfoAsset Manager/01 Ruby/0030 List Database Metadata/UI-ListCurrentNetworkFieldStructure.rb
@@ -0,0 +1,17 @@
+##Lists Object descriptive name & Object table name, followed by Field descriptive name, Fieldname, & type, followed by blob attributes (where relevant).
+on=WSApplication.current_network
+	on.tables.each do |i|
+		puts "****#{i.description}, #{i.name}"
+		i.fields.each do |j|
+			puts  "\t#{j.description}, #{j.name}, #{j.data_type}"
+			if j.data_type=='WSStructure'
+				if j.fields.nil?
+					puts "\t\t***badger***"
+				else
+					j.fields.each do |bf|
+						puts "\t\t#{bf.description}, #{bf.name}, #{bf.data_type}"
+				end
+			end
+		end
+	end
+end
