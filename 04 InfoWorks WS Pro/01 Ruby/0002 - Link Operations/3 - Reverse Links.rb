b12cb5bdc19a9b14817b424d22c1922f11c688fc
Dave Watts <dave.watts@autodesk.com>
2023-02-24 10:18:26 +0000

    Updated link scripts

--- 04 InfoWorks WS Pro/01 Ruby/0002 - Link Operations/3 - Reverse Links.rb
+++ 04 InfoWorks WS Pro/01 Ruby/0002 - Link Operations/3 - Reverse Links.rb
@@ -5,18 +5,16 @@ network = WSApplication.current_network
 network.transaction_begin
 
 network.row_objects_selection("_links").each do |link|
-	# Cache the bends (geometry of the line) - when we switch the US and DS nodes, WS Pro
-	# will automatically switch the first and last bends too, it's easier for us to ignore that
-	# and work from the original geometry
-	bends = link["bends"]
+	old_bends = link["bends"]
+	new_bends = Array.new
+	while !old_bends.empty?
+		new_bends.concat(old_bends.pop(2))
+	end
 
-	# Flip the Upstream and Downstream nodes
-	# We have to cache one of the values as we do this
-	old_id = link["us_node_id"]
+	old_us_id = link["us_node_id"]
 	link["us_node_id"] = link["ds_node_id"]
-	link["ds_node_id"] = old_id
-
-	link["bends"] = bends.rotate(2)
+	link["ds_node_id"] = old_us_id
+	link["bends"] = new_bends
 
 	link.write
 end
