f078d6a513932e50e08bad0523b4a41cf174d840
Krzysztof-T <80350828+Krzysztof-T@users.noreply.github.com>
2023-04-03 16:37:30 +0100

    include not nil

--- 02 InfoAsset Manager/01 Ruby/0001A ODEC Callback Examples/ODEC_Exporter_FilterFieldValue.rb
+++ 02 InfoAsset Manager/01 Ruby/0001A ODEC Callback Examples/ODEC_Exporter_FilterFieldValue.rb
@@ -2,9 +2,9 @@
 
 class Exporter
 	def Exporter.PipeConditionScore(obj)
-		if obj['condition_score_flag'] == 'KT1'						# If the field (flag) value is 'KT1'
-			return obj['condition_score']						# export the field value
-		else						# If the value doesn't match any of the above ifs
+		if obj['condition_score_flag'] == 'KT1' && !obj['condition_score'].nil?		# If the field (flag) value is 'KT1' and the related field is not null
+			return obj['condition_score']											# export the field value
+		else								# If the value doesn't match any of the above ifs
 			return nil						# export nil (null)
 		end
 	end
