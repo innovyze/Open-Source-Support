106856b9bea3e26aa9fe64776bf53c954c1b3e8d
Krzysztof Tchorzewski <krzysztof.tchorzewski@autodesk.com>
2023-09-05 12:05:59 +0100

    new script

---
+++ 02 InfoAsset Manager/01 Ruby/0034 Display Export geometries/UI-ExportPipeArrayCSV.rb
@@ -0,0 +1,17 @@
+## Display and Export to CSV the point_array of a Pipe to to the file on line 5.
+
+net=WSApplication.current_network
+require 'csv'
+CSV.open("c:\\temp\\pipes.csv", "wb") do |csv|
+	pipes=net.row_objects_selection('cams_pipe')
+	pipes.each do |s|
+		unless s.point_array.nil?
+			puts "#{s.us_node_id}.#{s.ds_node_id}.#{s.link_suffix} #{s.point_array}"
+			csv << ["#{s.us_node_id}", "#{s.ds_node_id}", "#{s.link_suffix}", "#{s.point_array}"]
+		else
+			puts "#{s.us_node_id}.#{s.ds_node_id}.#{s.link_suffix}"
+			csv << ["#{s.us_node_id}", "#{s.ds_node_id}", "#{s.link_suffix}",]	
+		end
+	end
+end
+
