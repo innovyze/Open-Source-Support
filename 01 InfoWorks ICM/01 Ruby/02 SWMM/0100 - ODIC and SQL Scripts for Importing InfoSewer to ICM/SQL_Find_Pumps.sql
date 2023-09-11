8793b3ccb2e39eff4e543006563863983c58d5cb
dickinsonre <robert.dickinson@gmail.com>
2023-09-12 07:21:30 +0800

    Updated SQL with many more comments for folder number 100

--- 01 InfoWorks ICM/01 Ruby/02 SWMM/0100 - ODIC and SQL Scripts for Importing InfoSewer to ICM/SQL_Find_Pumps.sql
+++ 01 InfoWorks ICM/01 Ruby/02 SWMM/0100 - ODIC and SQL Scripts for Importing InfoSewer to ICM/SQL_Find_Pumps.sql
@@ -3,12 +3,26 @@
 // Spatial Search: blank
 */
 
-/* Insert new records into the 'pump' table based on certain conditions in the 'link' table */
+/* 
+  Insert new records into the 'pump' table.
+  These records are based on certain conditions met in the 'link' table.
+*/
 INSERT INTO pump (us_node_id, ds_node_id, link_suffix, asset_id)
+
+/* 
+  Use SELECT to fetch the corresponding values from the 'link' table.
+  The link_suffix is set as '2' for each new record.
+*/
 SELECT us_node_id, ds_node_id, '2', asset_id
 
-/* FROM link  -- Source table for the SELECT */
-WHERE user_text_10 = 'Pump'; /* Condition to filter records in the source table */
+/* 
+  Source table for the SELECT query is 'link'.
+  Filter the records where the 'user_text_10' field is 'Pump'.
+*/
+WHERE user_text_10 = 'Pump';
 
-/* Delete records from the link table where user_text_10 equals 'Pump' */
+/* 
+  Delete records from the 'link' table where 'user_text_10' equals 'Pump'.
+  This helps to avoid duplicate or unnecessary records.
+*/
 DELETE WHERE user_text_10 = 'Pump';
--- 01 InfoWorks ICM/01 Ruby/02 SWMM/0100 - ODIC and SQL Scripts for Importing InfoSewer to ICM/SQL_Make_Subcatchments.sql
+++ 01 InfoWorks ICM/01 Ruby/02 SWMM/0100 - ODIC and SQL Scripts for Importing InfoSewer to ICM/SQL_Make_Subcatchments.sql
@@ -3,9 +3,25 @@
 // Spatial Search: blank
 */
 
-/* Insert new records into the 'subcatchment' table based on certain conditions in the 'node' table */
+/* 
+  Begin the process of populating the 'subcatchment' table with new records.
+  The new records are based on certain conditions met in the 'node' table.
+*/
 
-INSERT INTO subcatchment (subcatchment_id,node_id,total_area,x,y,connectivity)
-SELECT node_id, node_id, 0.10, x,y,100
+/* 
+  Use INSERT INTO to add new records to 'subcatchment' table.
+  The fields to be populated are: subcatchment_id, node_id, total_area, x, y, and connectivity.
+*/
+INSERT INTO subcatchment (subcatchment_id, node_id, total_area, x, y, connectivity)
 
+/* 
+  Use SELECT to fetch the corresponding values from the 'node' table.
+  The total_area is set as 0.10 and connectivity as 100 for each new record.
+*/
+SELECT node_id, node_id, 0.10, x, y, 100
+
+/* 
+  Filter the records from the 'node' table where the node_type is 'Manhole'.
+  Only records that meet this condition will be inserted into the 'subcatchment' table.
+*/
 WHERE node_type = 'Manhole';
