7218be39c2297aedfa2a4b154f37286e400cd4cf
kmaschmann <kate.maschmann@autodesk.com>
2024-06-28 09:15:21 -0500

    Updated SQL for use with parallel pumps

--- 01 InfoWorks ICM/01 Ruby/01 InfoWorks/0060  - ODIC and SQL, Ruby  Scripts for Importing InfoSewer to ICM/Find_Pumps.sql
+++ 01 InfoWorks ICM/01 Ruby/01 InfoWorks/0060  - ODIC and SQL, Ruby  Scripts for Importing InfoSewer to ICM/Find_Pumps.sql
@@ -1,28 +1,21 @@
-/* Update Pump Table
-// Object Type: All Pumps and Links
+/* Find_Pumps.sql
+// Object Type: All Links
 // Spatial Search: blank
 */
 
-/* 
-  Insert new records into the 'pump' table.
-  These records are based on certain conditions met in the 'link' table.
+/*
+//This SQL selects Conduits tagged as 'Pump', deletes their existing records from the Conduit table, and reinserts them into the Pump table
 */
+
+LIST $asset_id String;
+SELECT DISTINCT asset_id INTO $asset_id WHERE user_text_10 = 'Pump';
+LET $i = 1;
+WHILE $i <= LEN($asset_id);
+SELECT us_node_id INTO $us_node_id WHERE asset_id =AREF($i, $asset_id);
+SELECT ds_node_id INTO $ds_node_id WHERE asset_id =AREF($i, $asset_id);
+SELECT link_suffix into $link_suffix WHERE asset_id =AREF($i, $asset_id); 
+DELETE WHERE asset_id =AREF($i, $asset_id);
 INSERT INTO pump (us_node_id, ds_node_id, link_suffix, asset_id)
-
-/* 
-  Use SELECT to fetch the corresponding values from the 'link' table.
-  The link_suffix is set as '2' for each new record.
-*/
-SELECT us_node_id, ds_node_id, '2', asset_id
-
-/* 
-  Source table for the SELECT query is 'link'.
-  Filter the records where the 'user_text_10' field is 'Pump'.
-*/
-WHERE user_text_10 = 'Pump';
-
-/* 
-  Delete records from the 'link' table where 'user_text_10' equals 'Pump'.
-  This helps to avoid duplicate or unnecessary records.
-*/
-DELETE WHERE user_text_10 = 'Pump';
+VALUES($us_node_id, $ds_node_id, $link_suffix, AREF($i, $asset_id));
+LET $i = $i+1;
+WEND;
