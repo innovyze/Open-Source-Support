043670d6647f1488815f6c3d6cadfd2a90b555e0
kmaschmann <kate.maschmann@autodesk.com>
2024-06-28 15:44:13 -0500

    Removed unused scripts

--- 01 InfoWorks ICM/01 Ruby/01 InfoWorks/0060  - ODIC and SQL, Ruby  Scripts for Importing InfoSewer to ICM/SET convert population.sql
+++
@@ -1,7 +0,0 @@
-/* Make Subcatchments
-// Object Type: All Nodes and Subcatchments
-// Spatial Search: blank
-*/
-
-/* needed for per capita flow in WW profile of ft3/day */
-SET population = population/86400.0;
--- 01 InfoWorks ICM/01 Ruby/01 InfoWorks/0060  - ODIC and SQL, Ruby  Scripts for Importing InfoSewer to ICM/SET node_type = 'Break'.sql
+++
@@ -1,7 +0,0 @@
-/* Set Break Nodes
-//Object Type: All Nodes
-//Spatial Search: blank
-*/
-
-SET node_type = 'Break' WHERE user_text_3 = '1';
-SET user_text_10 = 'Break' WHERE user_text_3 = '1;
