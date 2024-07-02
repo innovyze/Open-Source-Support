3a9c9f13efbe8c176c62b8b039c6725eea9e13a2
kmaschmann <kate.maschmann@autodesk.com>
2024-07-02 13:31:13 -0500

    Clarifying updates

--- 01 InfoWorks ICM/01 Ruby/01 InfoWorks/0060  - ODIC and SQL, Ruby  Scripts for Importing InfoSewer to ICM/SET pump on and off.sql
+++ 01 InfoWorks ICM/01 Ruby/01 InfoWorks/0060  - ODIC and SQL, Ruby  Scripts for Importing InfoSewer to ICM/SET pump on and off.sql
@@ -1,13 +1,9 @@
 /* Set pump on and off
 // Object Type: Pump
-// Spatial Search: Nearest
-// Distance: 10
-// Layer Type: Network layer
-// Layer: Node
+// Spatial Search: blank
 */
 
-/*  Convert the switch_on_level and switch_off_level from depth to elevation based on the chamber floor elevation */
+/*  Convert the switch_on_level and switch_off_level from depth to elevation */
 
-
-SET switch_on_level = switch_on_level + spatial.chamber_floor,
-switch_off_level = switch_off_level + spatial.chamber_floor
+SET switch_on_level = switch_on_level + us_node.chamber_floor,
+switch_off_level = switch_off_level + us_node.chamber_floor
