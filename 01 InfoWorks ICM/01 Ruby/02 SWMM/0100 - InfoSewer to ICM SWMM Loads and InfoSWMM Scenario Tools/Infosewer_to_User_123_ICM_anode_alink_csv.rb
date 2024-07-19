66ffd6a532522bb38b595f15fa25f85fd90c5d40
dickinsonre_Autodesk <robert.dickinson@gmail.com>
2024-07-19 12:36:36 +0800

    mode folder 100 changes
    incorporated Kate M InfoSWMM to ICM SWMM changes

--- 01 InfoWorks ICM/01 Ruby/02 SWMM/0100 - InfoSewer to ICM SWMM Loads and InfoSWMM Scenario Tools/Infosewer_to_User_123_ICM_anode_alink_csv.rb
+++ 01 InfoWorks ICM/01 Ruby/02 SWMM/0100 - InfoSewer to ICM SWMM Loads and InfoSWMM Scenario Tools/Infosewer_to_User_123_ICM_anode_alink_csv.rb
@@ -1,101 +1,108 @@
+#This script can be used to create selection lists for active elements in each scenario (Scenario folder)
+#This script can also be used to create seleciton lists for each selection set from InfoSWMM (Selection folder)
+
+# Required libraries
 require 'csv'
 require 'pathname'
 
+# Method to import anode
 def import_anode(open_net, parent_object)
-  # Prompt the user to pick a folder
-  val = WSApplication.prompt("Facilty for an InfoSWMM Scenario", [
+  # Prompt the user for folder selection
+  val = WSApplication.prompt("Facility for an InfoSWMM Scenario", [
     ['Pick the Scenario Folder', 'String', nil, nil, 'FOLDER', 'Scenario Folder']
   ], false)
 
-  # Check if the user canceled the prompt
+  # End function if user cancels the prompt
   return if val.nil?
 
   folder_path = val[0]
-  puts "Folder path: #{folder_path}"
-  puts "If the CSV File is Empty - this means all nodes or links are active in the InfoSewer or InfoSWMM Scenario"
+  puts "Selected folder: #{folder_path}"
+  puts "\nNote: If the CSV File is empty, it means all nodes or links are active in the InfoSWMM Scenario."
+  puts "\n"
 
-  # Create a hash that maps id to row object for links, nodes, and subcatchments
+  # Hashes to store links, nodes, and subcatchments
   id_to_link = {}
   id_to_node = {}
   id_to_subcatchment = {}
 
-  open_net.row_objects('_links').each { |ro| id_to_link[ro.asset_id] = ro }
+  # Populate the hashes
+  open_net.row_objects('_links').each { |ro| id_to_link[ro.id] = ro }
   open_net.row_objects('_nodes').each { |ro| id_to_node[ro.node_id] = ro }
   open_net.row_objects('_subcatchments').each { |ro| id_to_subcatchment[ro.subcatchment_id] = ro }
 
-  # Iterate over all subdirectories in the given folder
+  # Iterate over all subdirectories in the folder
   Pathname.new(folder_path).children.select(&:directory?).each do |dir|
     ['anode.csv', 'alink.csv'].each do |filename|
-      puts Pathname.new(dir).basename
+      puts "Subdirectory: #{Pathname.new(dir).basename}"
       $selection_set = Pathname.new(dir).basename
-      puts filename
+      puts "Filename: #{filename}"
       csv_path = "#{dir}/#{filename}"
 
-      # Check if the CSV file exists in the subdirectory
+      # Check if the CSV file exists
       if File.exist?(csv_path)
         begin
+          # Try opening the file
           File.open(csv_path, 'a') {}
-          puts "Found #{filename} in #{csv_path}"
+          puts "Located #{filename} in #{csv_path}"
         rescue IOError => e
+          # Handle file open error
           puts "File #{filename} is already open"
         end
       end
-      # Initialize an empty array to hold the hashes
+
+      # Array to hold the CSV rows
       rows = []
 
       # Read the CSV file
-        CSV.foreach(csv_path, headers: true) do |row|
-          row_hash = row.to_h
-          #row_hash.delete("TYPE") # Remove the "TYPE" key-value pair
-          row_hash["dir_source"] = File.basename(dir.to_s) + "_" + File.basename(filename, '.*') # Combine 'dir' and 'source'
-          row_hash = row.to_h
-          rows << row_hash
-        end
+      CSV.foreach(csv_path, headers: true) do |row|
+        row_hash = row.to_h
+        # Add directory and source to the hash
+        row_hash["dir_source"] = File.basename(dir.to_s) + "_" + File.basename(filename, '.*')
+        row_hash = row.to_h
+        rows << row_hash
+      end
 
       puts "Row count: #{rows.count}"
 
+      # Update links, nodes, and subcatchments
       rows.each do |row|
         # Update links
-        ro = id_to_link[row["ID"]]
-        if ro
-          ro.asset_id_flag = 'ISAC'
+        if ro = id_to_link[row["ID"]]
+          ro.id_flag = 'ISAC'
           ro.write
         end
-    
+
         # Update nodes
-        ro = id_to_node[row["ID"]]
-        if ro
+        if ro = id_to_node[row["ID"]]
           ro.node_id_flag = 'ISAC'
           ro.write
         end
-    
+
         # Update subcatchments
-        ro = id_to_subcatchment[row["ID"]]
-        if ro
+        if ro = id_to_subcatchment[row["ID"]]
           ro.subcatchment_id_flag = 'ISAC'
           ro.write
         end
       end
     end
 
-  
+    # Clear selection and select new elements
     open_net.clear_selection
-
     open_net.run_SQL "_links","flags.value='ISAC'"
     open_net.run_SQL "_nodes","flags.value='ISAC'"
     open_net.run_SQL "_subcatchments","flags.value='ISAC'"
 
+    # Create and save selection list
     sl = parent_object.new_model_object 'Selection List', $selection_set.to_s
     puts s1 = sl.name
     open_net.save_selection sl
-
   end
-
 end
 
-# Access the current open network in the application
+# Access the current network
 open_net = WSApplication.current_network
 
+# Fetch the parent object
 db = WSApplication.current_database
 current_network = WSApplication.current_network
 current_network_object = current_network.model_object
@@ -109,10 +116,10 @@ rescue
   parent_object = db.model_object_from_type_and_id 'Model Group', parent_id
 end
 
-# Call the import_anode method
+# Start a transaction and call the import_anode method
 open_net.transaction_begin
-import_anode(open_net,parent_object)
+import_anode(open_net, parent_object)
 open_net.transaction_commit
 
-# Indicate the completion of the import process
-puts "Finished the Import of InfoSewer/InfoSWMM Facility Manager Active Elements to ICM InfoWorks Selection Sets" 
+# Print completion message
+puts "\nFinished the Import of InfoSWMM Facility Manager Active Elements to ICM SWMM Selection Lists"
