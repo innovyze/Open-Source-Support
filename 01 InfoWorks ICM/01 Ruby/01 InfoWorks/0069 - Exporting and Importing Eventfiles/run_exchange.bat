ec7c210b26e731117a8bf414e4b6501372a339bd
kmaschmann <kate.maschmann@autodesk.com>
2024-08-14 13:15:55 -0500

    Link Geometry Data

--- 01 InfoWorks ICM/01 Ruby/01 InfoWorks/0069 - Exporting and Importing Eventfiles/run_exchange.bat
+++
@@ -1,7 +0,0 @@
-echo off
-SET "ExchangePath=C:\Program Files\Autodesk\InfoWorks ICM Ultimate 2025\ICMExchange.exe"
-SET "RubyScriptPath=%~dp0main.rb"
-SET "ProductCode=/ICM"
-echo on
-"%ExchangePath%" "%RubyScriptPath%" %ProductCode%
-pause
