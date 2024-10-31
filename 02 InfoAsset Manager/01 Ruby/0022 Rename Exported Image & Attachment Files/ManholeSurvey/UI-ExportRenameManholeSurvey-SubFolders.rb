1fce35d66277089404c637354d29c98c4cf78c38
Krzysztof Tchorzewski <80350828+Krzysztof-T@users.noreply.github.com>
2024-10-31 17:00:25 +0000

    new script

--- 02 InfoAsset Manager/01 Ruby/0022 Rename Exported Image & Attachment Files/ManholeSurvey/UI-ExportRenameManholeSurvey-SubFolders.rb
+++ 02 InfoAsset Manager/01 Ruby/0022 Rename Exported Image & Attachment Files/ManholeSurvey/UI-ExportRenameManholeSurvey-SubFolders.rb
@@ -1,6 +1,7 @@
-#Exports images from Manhole Surveys to a chosen folder, renames them to the Original File Name or Description
+#Exports images from Manhole Surveys to a chosen folder, renames them to the Original File Name or Description, moves them to sub-folders based on the Survey id
 
 require 'csv'
+require 'fileutils'
 
 nw=WSApplication.current_network
 
@@ -19,7 +20,11 @@ exportfile=val[1].to_s
 exportname=val[2].to_s
 config = File.dirname(WSApplication.script_file)
 
-if val[0]==nil
+if val==nil
+	WSApplication.message_box("Parameters dialog closed\nScript cancelled",'OK','!',nil)
+	abort("Invalid parameters.")
+	exit
+elsif val[0]==nil
 	WSApplication.message_box("Files folder required\nScript cancelled",'OK','!',nil)
 elsif val[1]==nil
 	WSApplication.message_box("Mapping file required\nScript cancelled",'OK','!',nil)
@@ -44,27 +49,35 @@ nw.odec_export_ex('csv',config+'\\attachments.cfg',options,'ManholeSurvey',expor
 files = Dir.foreach(exportloc).select { |x| File.file?("#{exportloc}/#{x}") }
 found=[]
 files.each do |a|
-b=File.basename(a, ".*")
-found << b
+	b=File.basename(a, ".*")
+	found << b
 end
 
 CSV.foreach(exportfile, :headers=>true) do |row|
     if !row["db_ref"].nil?
-        if !row[exportname].nil?
-            fileFrom = File.join(exportloc, row["db_ref"])
-            filename = row[exportname]
-            fileTo = File.join(exportloc, filename.gsub(/[^0-9A-Za-z. _-]/, '') + File.extname(fileFrom))
-            
-            filenew = fileTo
-            unless found.include? filenew
-                File.rename(fileFrom, fileTo)
-                found << filenew
-            else
-                puts 'File "'+row["db_ref"]+'" not renamed, possible duplicate of "'+row[exportname]+'"'
-            end
-        else
-            puts 'File "'+row["db_ref"]+'" not renamed, no new name in column "'+exportname+'"'
-        end
+		if !row[exportname].nil?
+		
+			fileFolder = exportloc+'\\'+(row["id"].gsub(/[^0-9A-Za-z. _-]/, ''))
+			unless Dir.exists?(fileFolder)
+				Dir.mkdir(fileFolder)
+			end
+		
+			fileFrom = File.join(exportloc, row["db_ref"])
+			filename = row[exportname]
+			fileTo = File.join(fileFolder, filename.gsub(/[^0-9A-Za-z. _-]/, '') + File.extname(fileFrom))
+			
+			filenew = fileTo#row[exportname]
+			unless found.include? fileTo
+				File.rename(fileFrom, fileTo)
+				puts 'File "'+row["db_ref"]+'" >> "'+fileTo+'"'
+				found << fileTo
+			else
+				puts 'ERROR: File "'+row["db_ref"]+'" not renamed, possible duplicate of "'+row[exportname]+'"'
+				FileUtils.mv(fileFrom,File.join(fileFolder,row["db_ref"]))
+			end
+		else
+			puts 'ERROR: File "'+row["db_ref"]+'" not renamed, no new name in column "'+exportname+'"'
+		end
     end
 end
 
