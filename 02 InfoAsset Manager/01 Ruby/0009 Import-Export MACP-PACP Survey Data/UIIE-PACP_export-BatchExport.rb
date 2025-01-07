dbc5d0035c1eed2c8fba771440bebead23a20f31
Krzysztof Tchorzewski <80350828+Krzysztof-T@users.noreply.github.com>
2025-01-07 15:02:58 +0000

    updated to enable UI or IE

--- 02 InfoAsset Manager/01 Ruby/0009 Import-Export MACP-PACP Survey Data/UIIE-PACP_export-BatchExport.rb
+++ 02 InfoAsset Manager/01 Ruby/0009 Import-Export MACP-PACP Survey Data/UIIE-PACP_export-BatchExport.rb
@@ -1,10 +1,21 @@
 ## Export from a selection a specific amount of surveys to seperate files
-### Remove "_selection" from line #18 to run on all surveys
+### Remove "_selection" from line #16 to run on all surveys
 
 exportloc='C:\\Temp\\pacp_'		## Export folder/filename prefix
 outputmax=5						## How many Surveys per export file?
 
-net=WSApplication.current_network
+if WSApplication.ui?
+	net=WSApplication.current_network		## Uses current open network when run in UI
+else
+	db=WSApplication.open
+	dbnet=db.model_object_from_type_and_id 'Collection Network',2		## Run on Collection Network #2 in IE
+	net=dbnet.open
+end
+
+toProcess=Array.new
+net.row_objects_selection('cams_cctv_survey').each do |o|
+	toProcess << o.id
+end
 
 myHash=Hash.new
 myHash["Selection Only"]=true
@@ -12,12 +23,6 @@ myHash["Images"]=false
 myHash["Imperial"]=true
 myHash["InfoAsset"]=nil
 myHash["Format"]="7"
-
-
-toProcess=Array.new
-net.row_objects_selection('cams_cctv_survey').each do |o|
-	toProcess << o.id
-end
 
 total=toProcess.count
 count=0
