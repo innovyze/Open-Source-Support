e523abb3d741cfb08840229d38ee543901217156
dickinsonre_Autodesk <robert.dickinson@gmail.com>
2025-01-31 04:57:33 -0500

    Making Summary Tools for the first ten folders in ICM SWMM Ruby
    Making Summary Tools for the first ten items in ICM SWMM Ruby

--- 01 InfoWorks ICM/01 Ruby/02 SWMM/0134 - Input Message Box/Input Message Box.rb
+++ 01 InfoWorks ICM/01 Ruby/02 SWMM/0134 - Input Message Box/Input Message Box.rb
@@ -1,9 +1,19 @@
 # Modified from Innovyze Ruby Documentation
 
-output = WSApplication.input_box("Message box prompt line 1\nMessage box prompt line 2\nMessage Box prompt line 3", 'Message box title', 'Here is some initial text')
+# Display an input message box with a multi-line prompt and a title
+user_input = WSApplication.input_box(
+  "Once upon a time in the land of hydraulic modeling,\n" \
+  "there were two mighty kingdoms: SWMM5 and ICM InfoWorks.\n" \
+  "Both kingdoms thrived on managing water networks with great precision.\n" \
+  "One day, someone sought to unite their strengths in a single UX.\n" \
+  "What message would you send to the kingdoms to inspire this union?",
+  'A Tale of Two H&H Kingdoms',
+  'EPPA EPASWMM AND Wallingford Software in the Autodesk World!'
+)
 
-if output.nil?
-  puts "Cancel button hit, the input is: #{output}"
+# Check the user's response
+if user_input.nil?
+  puts "Cancel button hit"
 else
-  puts "OK button hit, the input is: #{output}"
+  puts "OK button hit, the input is: #{user_input}"
 end
