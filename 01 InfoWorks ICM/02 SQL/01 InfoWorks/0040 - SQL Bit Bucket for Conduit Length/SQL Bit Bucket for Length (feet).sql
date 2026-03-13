7814bd6b4c7254d3d5d724d464453bcd4e588387
kmaschmann <kate.maschmann@autodesk.com>
2026-03-13 15:07:10 -0500

    SQL/InfoWorks tidying

--- 01 InfoWorks ICM/02 SQL/01 InfoWorks/0040 - SQL Bit Bucket for Conduit Length/SQL Bit Bucket for Length (feet).sql
+++ 01 InfoWorks ICM/02 SQL/01 InfoWorks/0040 - SQL Bit Bucket for Conduit Length/SQL Bit Bucket for Length (feet).sql
@@ -3,5 +3,5 @@
 // Spatial Search: blank
 */
 
-LIST $buckets =  6,8,12,16,20,24,36,48,60,72,84,96,120,250,500,1000,2500,5000;
+LIST $buckets = 25,50,100,200,500,1000,2000,5000;
 SELECT count(*) AS 'Count' GROUP BY TITLE(RINDEX(conduit_length, $buckets), $buckets) AS 'Length (feet)'
